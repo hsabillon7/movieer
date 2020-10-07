d5095fbfcb8b2ddf015994a6acb604057a65e9b8
Héctor Sabillón <h.hsabillon@gmail.com>
2020-10-07 15:04:51 -0600

    Creación de la pantalla que muestra el listado de películas

---
+++ src/screens/MovieListScreen.js
@@ -0,0 +1,12 @@
+// Importar los módulos necesarios
+import React from "react";
+import { StyleSheet, Text } from "react-native";
+
+// Variable que contiene la pantalla (renderizar)
+const MovieListScreen = () => {
+  return <Text>Esta es la pantalla de listado de películas</Text>;
+};
+
+const styles = StyleSheet.create({});
+
+export default MovieListScreen;
