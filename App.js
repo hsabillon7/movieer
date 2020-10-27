26458d1a1e32f2af681e5a9ca54b3d7264cba216
Hector Sabillon <h.hsabillon@gmail.com>
2020-10-27 14:54:26 -0600

    Implementación del stack navigator para nuestra navegación

--- App.js
+++ App.js
@@ -1,6 +1,19 @@
 import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 import MovieListScreen from "./src/screens/MovieListScreen";
+import MovieSearchResults from "./src/screens/MovieSearchResults";
+
+// Crear nuestra navegación basada en stack (pilas)
+const Stack = createStackNavigator();
 
 export default function App() {
-  return <MovieListScreen></MovieListScreen>;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="movieList">
+        <Stack.Screen name="movieList" component={MovieListScreen} />
+        <Stack.Screen name="movieSearch" component={MovieSearchResults} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
